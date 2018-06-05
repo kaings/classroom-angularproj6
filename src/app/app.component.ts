0473f5ece0224618f8239540132ad2a605422bd9
willys_tjiawi <willys@huubap.com>
2018-06-05 11:18:07 +0800

    data service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Account } from './shared/account.model';
+import { DataService } from './shared/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DataService]
 })
-export class AppComponent {
-  accounts: Account[] = [
-    new Account('Test Account 1', 'Active'),
-    new Account('Test Account 2', 'Hidden')
-  ];
+export class AppComponent implements OnInit{
+  accounts: Account[] = [];
+  constructor(private dataService: DataService) {}
 
+  ngOnInit() {
+    this.accounts = this.dataService.accounts;
+  }
+
+  /*
   onStatusUpdate(updateInfo: {id: number, status: string}) {
     // console.log(this.accounts[updateInfo.id]);
-    this.accounts[updateInfo.id].status = updateInfo.status;
+    // this.accounts[updateInfo.id].status = updateInfo.status;
   }
 
   onAccountAdded(newAccountInfo: {name: string, status: string}){
-    this.accounts.push(newAccountInfo);
+    // this.accounts.push(newAccountInfo);
   }
 
   onAccountRemove(accountId: number) {
-    this.accounts.splice(accountId,1);
+    // this.accounts.splice(accountId,1);
   }
+  */
 }
